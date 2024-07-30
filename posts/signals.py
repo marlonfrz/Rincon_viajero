ced660443b0d6817a2c1a7bd5f546df7ef6d2134
marlonfrz <marlonfh2b1112@gmail.com>
2024-07-30 13:53:33 +0100

    photo format to webp added

--- posts/signals.py
+++ posts/signals.py
@@ -1,7 +1,10 @@
+import io
 import os
 
+from django.core.files.base import ContentFile
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
+from PIL import Image
 
 from .models import TravelPost
 
@@ -17,3 +20,18 @@ def delete_travelpost_image(sender, instance, **kwargs):
 def delete_cancelled_post(sender, instance, **kwargs):
     if instance.status == TravelPost.Status.CANCELLED:
         instance.delete()
+
+
+@receiver(post_save, sender=TravelPost)
+def convert_image_to_webp(sender, instance, **kwargs):
+    if instance.photo and not instance.photo.name.endswith('.webp'):
+        image = Image.open(instance.photo)
+
+        buffer = io.BytesIO()
+        image.save(buffer, format='WEBP')
+        webp_image = ContentFile(
+            buffer.getvalue(), name=instance.photo.name.replace('.jpg', '.webp')
+        )
+
+        instance.photo.save(instance.photo.name.replace('.jpg', '.webp'), webp_image, save=False)
+        instance.save()
